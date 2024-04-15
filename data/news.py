6e57fcfe134c2e55d91a8147d7cc73943674e68b
skuf <vjeje52@gmail.com>
2024-04-15 23:29:49 +1000

    модель для новостей

---
+++ data/news.py
@@ -0,0 +1,24 @@
+import datetime
+import sqlalchemy
+from sqlalchemy import orm
+from .db_session import SqlAlchemyBase
+
+
+class News(SqlAlchemyBase):
+    __tablename__ = "news"
+
+    id = sqlalchemy.Column(sqlalchemy.Integer,
+                           primary_key=True, autoincrement=True)
+    user_id = sqlalchemy.Column(sqlalchemy.Integer,
+                                sqlalchemy.ForeignKey("users.id"))
+    user = orm.relationship('User')
+    title = sqlalchemy.Column(sqlalchemy.String,
+                              nullable=True)
+    text = sqlalchemy.Column(sqlalchemy.String,
+                             nullable=True)
+    created_date = sqlalchemy.Column(sqlalchemy.DateTime,
+                                     default=datetime.datetime.now)
+    image = sqlalchemy.Column(sqlalchemy.String,
+                              nullable=True)
+
+
